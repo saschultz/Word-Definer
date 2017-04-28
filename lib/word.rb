b6be707790967a23f1706c17fcb9262fcef28972
Sara Schultz <saschultz95@gmail.com>
2017-04-28 08:32:19 -0700

    pass spec to return an initially empty array in word_spec.rb

--- lib/word.rb
+++ lib/word.rb
@@ -1,6 +1,11 @@
 class Word
   attr_accessor(:name)
+  
   def initialize(attributes)
     @name = attributes.fetch(:name)
+  end
+
+  def words
+    @@words = []
   end
 end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -2,10 +2,17 @@ require 'rspec'
 require 'word'
 
 describe 'Word' do
-  describe '#word' do
+  describe '#name' do
     it 'returns the name of the word' do
       test_word = Word.new({:name => 'serendipity'})
     expect(test_word.name).to(eq'serendipity')
+    end
+  end
+
+  describe '#words' do
+    it 'initially returns an empty array of words' do
+      test_word = Word.new({:name => 'serendipity'})
+    expect(test_word.words).to(eq([]))
     end
   end
 end
