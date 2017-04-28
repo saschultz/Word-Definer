c1d0a6c5c00a400d121c9cd672a753d0fdb7db8c
Sara Schultz <saschultz95@gmail.com>
2017-04-28 13:13:48 -0700

    WIP displaying added definitions.

--- app.rb
+++ app.rb
@@ -10,13 +10,27 @@ end
 
 post '/dictionary' do
   word = params.fetch('word')
-  @word = Word.new({name: word})
-  @word.save
+  Word.new({name: word}).save
   @dictionary = Word.all
   redirect '/'
   erb :index
 end
 
 get '/definitions/:id' do
+  @word = Word.find(params.fetch('id').to_i)
+  erb :definitions
+end
+
+get '/definitions/:id/added_content' do
+  @word = Word.find(params.fetch('id').to_i)
+  erb :definitions
+end
+
+post '/definitions/:id/added_content' do
+  definition = params.fetch('definition')
+  @definition = Definition.new({content: definition}).save
+  @word = Word.find(params.fetch('word_id').to_i)
+  @word.add_definition(@definition)
+  redirect '/definitions/:id'
   erb :definitions
 end
