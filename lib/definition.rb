bda1ab4e2a974bbc20c11a72869f43e0a924653c
Sara Schultz <saschultz95@gmail.com>
2017-04-28 11:27:37 -0700

    pass spec to find definition by its id number

--- lib/definition.rb
+++ lib/definition.rb
@@ -20,4 +20,14 @@ class Definition
     @@definitions = []
   end
 
+  def Definition.find(id)
+    found_definition = nil
+    @@definitions.each() do |definition|
+      if definition.id().eql?(id.to_i)
+        found_definition = definition
+      end
+    end
+    found_definition    
+  end
+
 end
--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -1,39 +1,55 @@
 require 'rspec'
 require 'definition'
 
-describe '#content' do
-  it 'returns the content of the definition' do
-    test_definition = Definition.new({:content => 'the occurrence and development of events by chance in a happy or beneficial way.'})
-  expect(test_definition.content).to(eq'the occurrence and development of events by chance in a happy or beneficial way.')
-  end
-end
-
-describe '.all' do
-  it 'initially returns an empty array of all definitions' do
-    Definition.new({:content => 'the occurrence and development of events by chance in a happy or beneficial way.'})
-  expect(Definition.all).to(eq([]))
-  end
-end
-
-describe '#save' do
-  it 'saves a definition to the array of definitions' do
-    test_definition = Definition.new({:content => 'the occurrence and development of events by chance in a happy or beneficial way.'})
-    test_definition.save
-  expect(Definition.all).to(eq([test_definition]))
-  end
-end
-
-describe '.clear' do
-  it 'empties out all the saved definitions' do
-    Definition.new({:content => 'the occurrence and development of events by chance in a happy or beneficial way.'})
+describe 'Definition' do
+  before() do
     Definition.clear
-  expect(Definition.all).to(eq([]))
   end
-end
 
-describe '#id' do
-  it 'returns the id of the definition' do
-    test_definition = Definition.new({:content => 'the occurrence and development of events by chance in a happy or beneficial way.'})
-  expect(test_definition.id).to(eq(1))  
+  describe '#content' do
+    it 'returns the content of the definition' do
+      test_definition = Definition.new({:content => 'the occurrence and development of events by chance in a happy or beneficial way.'})
+    expect(test_definition.content).to(eq'the occurrence and development of events by chance in a happy or beneficial way.')
+    end
+  end
+
+  describe '.all' do
+    it 'initially returns an empty array of all definitions' do
+      Definition.new({:content => 'the occurrence and development of events by chance in a happy or beneficial way.'})
+    expect(Definition.all).to(eq([]))
+    end
+  end
+
+  describe '#save' do
+    it 'saves a definition to the array of definitions' do
+      test_definition = Definition.new({:content => 'the occurrence and development of events by chance in a happy or beneficial way.'})
+      test_definition.save
+    expect(Definition.all).to(eq([test_definition]))
+    end
+  end
+
+  describe '.clear' do
+    it 'empties out all the saved definitions' do
+      Definition.new({:content => 'the occurrence and development of events by chance in a happy or beneficial way.'})
+      Definition.clear
+    expect(Definition.all).to(eq([]))
+    end
+  end
+
+  describe '#id' do
+    it 'returns the id of the definition' do
+      test_definition = Definition.new({:content => 'the occurrence and development of events by chance in a happy or beneficial way.'})
+    expect(test_definition.id).to(eq(1))
+    end
+  end
+
+  describe '.find' do
+    it 'returns a definition by its id number' do
+      test_definition_1 = Definition.new({:content => 'the occurrence and development of events by chance in a happy or beneficial way.'})
+      test_definition_1.save
+      test_definition_2 = Definition.new({:content => 'making unhappy, unlucky and unexpected discoveries occurring by design'})
+      test_definition_2.save
+    expect(Definition.find(test_definition_2.id)).to(eq(test_definition_2))  
+    end
   end
 end
