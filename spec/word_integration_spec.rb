44789d0cd7ac434e1c8a71175494445904620676
Sara Schultz <saschultz95@gmail.com>
2017-04-28 10:13:23 -0700

    pass integration specfor processing the user entry in word_integration_spec.rb

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -2,3 +2,12 @@ require 'capybara/rspec'
 require './app'
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
+
+describe 'the word path', {:type => :feature} do
+  it 'processes the user entry and returns the word' do
+    visit '/'
+    fill_in 'word', :with => 'heart'
+    click_button 'Add!'
+    expect(page).to have_content 'heart'
+  end
+end
